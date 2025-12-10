5ee0afcf00993fe3dea3988a02b2b832c332f6c2
Deepak Jaipal <deepakjaipal@gmail.com>
2025-12-10 15:44:49 +0530

    cors_error_1

--- Frontend/lib/api.ts
+++ Frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://new-ave-catering.vercel.app/api", 
+  baseURL: "https://ave-rg2r.onrender.com/api", 
   // headers: {
   //   "Content-Type": "application/json",
   // },
